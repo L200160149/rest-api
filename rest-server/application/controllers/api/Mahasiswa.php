870611b3723abef98fbf51dd443143862fb5844e
L200160149 <ums.informatika@gmail.com>
2021-02-21 17:53:19 +0700

    video 8 selesai

--- rest-server/application/controllers/api/Mahasiswa.php
+++ rest-server/application/controllers/api/Mahasiswa.php
@@ -15,6 +15,7 @@ class Mahasiswa extends REST_Controller
     {
         parent::__construct();
         $this->load->model('Mahasiswa_model');
+        $this->methods['index_get']['limit'] = 2;
     }
 
     public function index_get()
@@ -63,6 +64,53 @@ class Mahasiswa extends REST_Controller
                     'message' => 'id not found'
                 ], REST_Controller::HTTP_BAD_REQUEST);
             }
+        }
+    }
+
+    public function index_post()
+    {
+        $data = [
+            'nrp' => $this->post('nrp'),
+            'nama' => $this->post('nama'),
+            'email' => $this->post('email'),
+            'jurusan' => $this->post('jurusan')
+        ];
+
+        if ( $this->Mahasiswa_model->createMahasiswa($data) > 0)
+        {
+            $this->response([
+                    'status' => TRUE,
+                    'message' => 'new mahasiswa has been created'
+                ], REST_Controller::HTTP_CREATED);
+        } else {
+             $this->response([
+                    'status' => FALSE,
+                    'message' => 'failed to create new data !'
+                ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function index_put()
+    {
+        $id = $this->put('id');
+        $data = [
+            'nrp' => $this->post('nrp'),
+            'nama' => $this->post('nama'),
+            'email' => $this->post('email'),
+            'jurusan' => $this->post('jurusan')
+        ];
+
+        if ( $this->Mahasiswa_model->updateMahasiswa($data, $id) > 0)
+        {
+            $this->response([
+                    'status' => TRUE,
+                    'message' => 'mahasiswa has been updated'
+                ], REST_Controller::HTTP_NO_CONTENT);
+        } else {
+             $this->response([
+                    'status' => FALSE,
+                    'message' => 'failed to update data !'
+                ], REST_Controller::HTTP_BAD_REQUEST);
         }
     }
 }
